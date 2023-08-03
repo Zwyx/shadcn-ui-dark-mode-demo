ce555a85f6cf1039cdca29d0fcad8959ec4a7f22
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-08-04 00:00:57 +0800

    Fix 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Metadata } from "next";
+import { Head, Html, Main, NextScript } from "next/document";
+
+export const metadata: Metadata = {
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+};
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
